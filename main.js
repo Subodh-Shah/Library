647aa98e0bbe368f39e0aaa3d6159781b26a930c
Subodh Shah <shahsubodh166@gmail.com>
2024-11-08 15:26:42 +0545

    changed files from mjs to js

---
+++ main.js
@@ -0,0 +1,6 @@
+import Book from './components/books.js';
+
+const book = new Book('Loki', 'Tim', 2018, 300, 55);
+console.log(book);
+const form = document.getElementById('bookForm');
+console.log(form);
--- main.mjs
+++
@@ -1,4 +0,0 @@
-import Book from './components/books.mjs';
-
-const book = new Book('Loki', 'Tim', 2018, 300, 55);
-console.log(book);
