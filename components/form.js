8c29841a9e9fa20b1ef895acfb74b72e4d1243e6
Subodh Shah <shahsubodh166@gmail.com>
2024-11-08 22:08:34 +0545

    add some changes to add button

--- components/form.js
+++ components/form.js
@@ -1,28 +1,32 @@
 import Book from './Book.js';
 
 export default function bookComponent() {
-	const addButton = document.querySelector('#toggle-form');
+	const toggleButton = document.querySelector('#toggle-form');
 	const form = document.getElementById('bookForm');
 	const closeButton = document.getElementById('close-button');
-	const addBook = document.querySelector('.book-form');
+	const bookFormWrapper = document.querySelector('.book-form-wrapper');
 
-	addButton.addEventListener('click', () => {
-		addBook.style.display = 'block';
+	toggleButton.addEventListener('click', () => {
+		bookFormWrapper.style.display = 'block';
 	});
 
 	closeButton.addEventListener('click', () => {
-		addBook.style.display = 'none';
+		bookFormWrapper.style.display = 'none';
 	});
 
 	form.addEventListener('submit', (event) => {
 		event.preventDefault();
-		const title = document.getElementById('title').value;
-		const author = document.getElementById('author').value;
-		const pages = document.getElementById('pages').value;
-		const status = document.querySelector(
+		let title = document.getElementById('title').value;
+		let author = document.getElementById('author').value;
+		let pages = document.getElementById('pages').value;
+		let status = document.querySelector(
 			'input[name="status"]:checked'
 		).value;
 		const book = new Book(title, author, pages, status);
 		localStorage.setItem('Book', JSON.stringify(book));
+		document.getElementById('title').value = '';
+		document.getElementById('author').value = '';
+		document.getElementById('pages').value = '';
+		document.querySelector('input[name="status"]:checked').checked = false;
 	});
 }
